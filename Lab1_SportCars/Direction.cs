95ff6ac48682f27271566df654d2af8cdd4d38d8
Aleksey Kandalintsev <a.kandalintsev@mediasoft.team>
2021-02-21 20:54:57 +0400

    wip: sport car

---
+++ Lab1_SportCars/Direction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_SportCars
+{
+    /// <summary>
+    /// Направленеи движения автомобиля
+    /// </summary>
+    public enum Direction
+    {
+        Up,
+
+        Down,
+
+        Left,
+
+        Right
+    }
+}
---
+++ Lab1_SportCars/FormCar.cs
@@ -0,0 +1,75 @@
+﻿using Lab1SportCars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_SportCars
+{
+    public partial class FormCar : Form
+    {
+
+        private SportCar _car;
+        public FormCar()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Метод отрисовки т/с
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxCars.Width, pictureBoxCars.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            _car?.DrawTransport(gr);
+            pictureBoxCars.Image = bmp;
+        }
+
+
+        /// <summary>
+        /// Обработка нажатия кнопки "Создать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            _car = new SportCar();
+            _car.Init(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue, Color.Yellow, true, true, true, true);
+            _car.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxCars.Width, pictureBoxCars.Height);
+            Draw();
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопок управления
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    _car?.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    _car?.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    _car?.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    _car?.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+    }
+}
